599b2e67f8dd50c76e1bcbb2f169a923d53c3cbe
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-20 18:52:45 -0300

    refactor(noteform): separating the new notes form logic into a new component

---
+++ src/components/NoteForm/NoteForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const NoteForm = ({ createNote }) => {
+  const [newNote, setNewNote] = useState('');
+
+  const handleChange = ({ target }) => {
+    setNewNote(target.value);
+  };
+
+  const addNote = (e) => {
+    e.preventDefault();
+    createNote({
+      content: newNote,
+      important: Math.random() > 0.5,
+    });
+
+    setNewNote('');
+  };
+
+  return (
+    <div>
+      <h2>Create a new note</h2>
+      <form onSubmit={addNote}>
+        <input type='text' value={newNote} onChange={handleChange} />
+        <button type='submit'>save</button>
+      </form>
+    </div>
+  );
+};
+
+export default NoteForm;
+
+NoteForm.propTypes = {
+  createNote: PropTypes.func.isRequired,
+};

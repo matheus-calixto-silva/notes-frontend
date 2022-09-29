797eb61cd8eba547c0159273aee68e373833ca52
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-29 18:19:48 -0300

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,34 @@
+/* eslint-env node */
 module.exports = {
-  globals: {
-    process: true,
-  },
   env: {
     browser: true,
-    commonjs: true,
-    es2021: true,
+    es6: true,
+    'jest/globals': true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: ['react', 'jest'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
